7b46afc6164bf86136174a7198f0d6ea48b5034e
Pol Rivero <65060696+pol-rivero@users.noreply.github.com>
2025-04-09 23:21:47 +0200

    Fix CI

--- vendor/desktop-trampoline/script/test.mjs
+++ vendor/desktop-trampoline/script/test.mjs
@@ -1,9 +1,16 @@
 import { spawn } from 'child_process'
-import { join } from 'path'
+import { join, dirname } from 'path'
 import { readdir } from 'fs/promises'
 
 function reporter(r) {
   return ['--test-reporter', r, '--test-reporter-destination', 'stdout']
+}
+
+const thisFile = import.meta.url.replace('file://', '')
+if (!thisFile.endsWith('desktop-trampoline')) {
+  const scriptDir = dirname(thisFile)
+  const desktopTrampolineDir = dirname(scriptDir)
+  process.chdir(desktopTrampolineDir)
 }
 
 const files = await readdir('test', { recursive: true }).then(x =>
